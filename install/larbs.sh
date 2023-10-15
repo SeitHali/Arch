954aa9fb43ccc189abc792946cc7a697a59aa77f
Seythalilev Roman <seithalilev@gmail.com>
2023-10-15 11:55:08 +0300

    delete sed

--- install/larbs.sh
+++ install/larbs.sh
@@ -152,8 +152,7 @@ pipinstall() {
 }
 
 installationloop() {
-	([ -f $progsfile ] && cp $progsfile /tmp/progs.csv) ||
-		curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
+	([ -f $progsfile ] && cp $progsfile /tmp/progs.csv) || curl -Ls "$progsfile"
 	total=$(wc -l </tmp/progs.csv)
 	aurinstalled=$(pacman -Qqm)
 	while IFS=, read -r tag program comment; do
